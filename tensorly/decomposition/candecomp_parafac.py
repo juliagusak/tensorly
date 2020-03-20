Add: fix scales and zero_points computation

tensorly/decomposition/candecomp_parafac.py
@@ -496,7 +496,7 @@ def quantized_parafac(tensor, rank, n_iter_max, init,\
     random_state : {None, int, np.random.RandomState}
     verbose : int, optional
         Level of verbosity
-    stop_criterion : {'rec_error_deviation', 'rec_error_decrease'}, optional
+    stop_criterion : {'rec_error_deviation', 'rec_error_decrease', 'both_error_decrease'}, optional
        Stopping criterion if `tol` is not None
        
     qmodes : list
@@ -521,8 +521,6 @@ def quantized_parafac(tensor, rank, n_iter_max, init,\
         Active when `qscheme` is not None.
         If dim is not None, along the dimension `dim` the values in the `tensor` are scaled and offset by a different value (effectively the scale and offset become vectors).
         If dim is None, all values in the `tensor` are scaled and offset by the same value.
-
-
     Returns
     -------
     factors : List of factors of the CP decomposition element `i` is of shape
@@ -538,9 +536,8 @@ def quantized_parafac(tensor, rank, n_iter_max, init,\
 
     .. [2] Tomasi, Giorgio, and Rasmus Bro. "PARAFAC and missing values."
             Chemometrics and Intelligent Laboratory Systems 75.2 (2005): 163-180.
-
-
     """
+    
     import torch
     tl.set_backend('pytorch')
 
@@ -557,7 +554,7 @@ def quantized_parafac(tensor, rank, n_iter_max, init,\
                                  init = init, svd = svd,\
                                  random_state = random_state,
                                  normalize_factors=normalize_factors)
-    weights = tl.ones(rank, **tl.context(tensor))
+    weights = tl.ones( rank, **tl.context(tensor))
     
     ## Initialize quantized factors
     qfactors = copy.deepcopy(factors)
@@ -579,13 +576,12 @@ def quantized_parafac(tensor, rank, n_iter_max, init,\
                 
             qfactors[i] = qfactor
 
-
     rec_errors = []
     qrec_errors = []
     norm_tensor = float(tl.norm(tensor, 2))
     
-    rec_error_flag = return_rec_errors or stop_criterion.startswith('rec')
-    qrec_error_flag = return_qrec_errors or stop_criterion.startswith('qrec')
+    rec_error_flag = return_rec_errors or stop_criterion.startswith('rec') or stop_criterion.startswith('both')
+    qrec_error_flag = return_qrec_errors or stop_criterion.startswith('qrec') or stop_criterion.startswith('both')
 
     if verbose:
         print("In parafac norm_tensor = {}".format(norm_tensor))
@@ -621,7 +617,8 @@ def quantized_parafac(tensor, rank, n_iter_max, init,\
                             
                         pseudo_inverse = pseudo_inverse * tl.dot(
                             tl.transpose(factor), factor)
-                        
+                
+                
                 mttkrp = unfolding_dot_khatri_rao(tensor,\
                                                   (None, list(map(lambda i : qfactors[i] if qflag and (i in qi) else factors[i], range(ndim)))\
                                                   ), mode)
@@ -657,24 +654,36 @@ def quantized_parafac(tensor, rank, n_iter_max, init,\
                         qfactors[mode] = qfactor
                     else:
                         qfactors[mode] = factor
-                    
-
+            
+            weights_prod = copy.deepcopy(weights)
+            qweights_prod = copy.deepcopy(weights)
+                        
+            if normalize_factors and qflag:
+                for i in range(len(factors)):
+                    if (i in qmodes):
+                        nrm = tl.norm(qfactors[i], order = 2, axis = 0)
+                        qweights_prod /= nrm
+                        if i in qi:
+                            weights_prod /= nrm
+            
             
             if rec_error_flag:
                 # ||tensor - rec||^2 = ||tensor||^2 + ||rec||^2 - 2*<tensor, rec>
-                factors_norm = kruskal_norm((weights, factors))
+#                 factors_norm = kruskal_norm((weights_prod,
+#                                             list(map(lambda i : qfactors[i] if qflag and (i in qi) else factors[i], range(ndim)))
+#                                             ))
 
                 # mttkrp and factor for the last mode. This is equivalent to the
                 # inner product <tensor, factorization>
-                iprod = tl.sum(tl.sum(mttkrp*factor, axis=0)*weights)
-                unnorml_rec_error = tl.sqrt(tl.abs(norm_tensor**2 + factors_norm**2 - 2*iprod))
+#                 iprod = tl.sum(tl.sum(mttkrp*factor, axis=0)*weights_prod)
+#                 unnorml_rec_error = tl.sqrt(tl.abs(norm_tensor**2 + factors_norm**2 - 2*iprod))
 
-#                 unnorml_rec_error = float(tl.norm(tensor - tl.kruskal_to_tensor((weights, factors)), 2))            
+                unnorml_rec_error = float(tl.norm(tensor - tl.kruskal_to_tensor((weights, factors)), 2))            
                 rec_error = unnorml_rec_error / norm_tensor
                 rec_errors.append(rec_error)
 
             if qrec_error_flag:
-                qunnorml_rec_error = float(tl.norm(tensor - tl.kruskal_to_tensor((weights, qfactors)), 2))
+                qunnorml_rec_error = float(tl.norm(tensor - tl.kruskal_to_tensor((qweights_prod, qfactors)), 2))
                 qrec_error = qunnorml_rec_error / norm_tensor
                 qrec_errors.append(qrec_error)
 
@@ -700,6 +709,10 @@ def quantized_parafac(tensor, rank, n_iter_max, init,\
                         stop_flag = abs(qrec_error_decrease) < tol
                     elif stop_criterion == 'qrec_error_decrease':
                         stop_flag =  qrec_error_decrease < tol
+                    elif stop_criterion == 'both_error_deviation':
+                        stop_flag = (abs(rec_error_decrease) < tol) and (abs(qrec_error_decrease) < tol)
+                    elif stop_criterion == 'both_error_decrease':
+                        stop_flag =  (rec_error_decrease < tol) and (qrec_error_decrease < tol)
                     else:
                         raise TypeError("Unknown stop criterion")
 
@@ -716,7 +729,7 @@ def quantized_parafac(tensor, rank, n_iter_max, init,\
                             
                             
     kruskal_tensor = KruskalTensor((weights, factors))
-    qkruskal_tensor = KruskalTensor((weights, qfactors))
+    qkruskal_tensor = KruskalTensor((qweights_prod, qfactors))
                             
     if return_scale_zeropoint:
         return (kruskal_tensor, qkruskal_tensor), (tuple(rec_errors), tuple(qrec_errors)), tuple(factors_scale), tuple(factors_zeropoint)

tensorly/quantization.py
@@ -2,55 +2,43 @@ import tensorly as tl
 from .base import unfold
 
 import torch
+import math
 
-def get_scale_denominator(dtype):
-    """Returns a number of intervals into which we divide
-    a range of valid values [min_value, max_malue] during quantization.
-    
-    Parameters
-    ----------
-    dtype : torch.dtype
-        Type to which we cast during quantization.
-    
-    Returns
-    -------
-    int
-        Number of quantization intervals.
-    """
-    
-    if dtype == torch.qint8:
-        scale_denom = 2**8 - 1  
-    elif dtype == torch.qint32:
-        scale_denom = 2**32 - 1
-    else:
-        raise TypeError("Can't perform quantization. Unknown quantization type: {}".format(dtype))
-        return
-    
-    return scale_denom
-
-
-def get_per_channel_stats(tensor, mode = 0):  
-    """Returns max, min and mean along last dimension for mode-`mode` unfolding
-    of `tensor` with modes starting at `0`.
+def get_tensor_stats(tensor, qscheme, mode = 0):  
+    """Returns max and min along last dimension for mode-`mode` unfolding
+    of `tensor` with modes starting at `0`, if qscheme is per channel.
+    If qscheme is per tensor,  returns max and min computed across all elements.
     
     Parameters
     ----------
     tensor : ndarray
+    qscheme : quantization scheme, default is ``torch.per_tensor_affine``
+        Has to be one of: ``torch.per_tensor_affine``, ``torch.per_tensor_symmetric``, ``torch.per_channel_affine``, ``torch.per_channel_symmetric``
     mode : int, default is 0
           Indexing starts at 0, therefore mode is in ``range(0, tensor.ndim)``.
 
     Returns
     -------
     tuple
-        max, min, mean of unfolded_tensor along last dimension.
+        max, min of unfolded_tensor along last dimension.
     """
-    unfolded_tensor = unfold(tensor, mode = mode)
+    if qscheme in [torch.per_channel_affine, torch.per_channel_symmetric]:
+        unfolded_tensor = unfold(tensor, mode = mode)
+
+        tmax = unfolded_tensor.max(dim = -1)[0]
+        tmin = unfolded_tensor.min(dim = -1)[0]
+        #tmean = unfolded_tensor.mean(dim = -1)
+        
+    elif qscheme in [torch.per_tensor_affine, torch.per_tensor_symmetric]:
+        tmax = tensor.max()
+        tmin = tensor.min()
+        
+    else:
+        raise TypeError("Can't collect statistics. Unknown quantization scheme: {}".format(qscheme))
+        return
+   
     
-    tmax = unfolded_tensor.max(dim = -1)[0]
-    tmin = unfolded_tensor.min(dim = -1)[0]
-    tmean = unfolded_tensor.mean(dim = -1)
-    
-    return tmax, tmin, tmean
+    return tmax, tmin
 
 
 def get_scale_zeropoint(tensor,\
@@ -80,37 +68,37 @@ def get_scale_zeropoint(tensor,\
         Offset in integer value that maps to float zero.
     """
     
-    scale_denom = get_scale_denominator(dtype)
-  
-    if qscheme in [torch.per_channel_affine, torch.per_channel_symmetric]:
+    #scale_denom = qmax - qmin, where qmax = 2**(nbits-1) - 1, qmin = 2**(nbits - 1)
+    if dtype == torch.qint8:
+        q = 2**7
+        scale_denom = 2*q - 1 
+    elif dtype == torch.qint32:
+        q = 2**31
+        scale_denom = 2*q - 1
+    else:
+        raise TypeError("Can't perform quantization. Unknown quantization type: {}".format(dtype))
+        return
+    
+    
+    tmax, tmin = get_tensor_stats(tensor, qscheme, mode = dim)
+    
+    if qscheme in [torch.per_channel_symmetric, torch.per_tensor_symmetric]:
+        scale = 2 * torch.where(tmin.abs() > tmax, tmin.abs(), tmax) / scale_denom
         
-        tmax, tmin, tmean = get_per_channel_stats(tensor, mode = dim)
-        
-        if qscheme == torch.per_channel_symmetric:
-            zero_point = tmean.round()
+        if dtype == torch.qint8:
+            zero_point = torch.zeros(scale.shape).int()
         else:
-            zero_point = tl.where(tmin < 0, tmin, tl.zeros(tmin.shape)).floor()
-            tmin = tl.where(tmin < zero_point, tmin, zero_point)
-            
-        zero_point = zero_point.int()    
-
-    elif qscheme in [torch.per_tensor_affine, torch.per_tensor_symmetric]:
-        tmax = tensor.max()
-        tmin = tensor.min() 
+            zero_point = (torch.zeros(scale.shape) + 128).int()
         
-        if qscheme == torch.per_tensor_symmetric:
-            zero_point = tensor.mean().round()
-        else:
-            zero_point = min(tmin, 0).floor().int()
-            tmin = min(tmin, zero_point)
-            
-        zero_point = zero_point.int()
+    elif qscheme in [torch.per_channel_affine, torch.per_tensor_affine]:
+        scale = (tmax - tmin) / scale_denom 
+        zero_point = (-q - (tmin / scale).int()).int()
         
+        zero_point = torch.clamp(zero_point, -q, q - 1)
     else:
         raise TypeError("Can't perform quantization. Unknown quantization scheme: {}".format(qscheme))
         return
 
-    scale = (tmax - tmin)/scale_denom 
     return scale, zero_point 
     
     